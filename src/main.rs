e36ae3b1b7446bd26ab4d599528650dec715f009
Salmaan Saeed <salsaeed999@gmail.com>
2022-09-04 23:31:18 -0400

    Created html templates for the home, about, and 404 pages with the tera template engine.

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,42 @@
 
 #[macro_use] extern crate rocket;
 
+#[macro_use] extern crate serde_derive;
+extern crate serde_json;
+extern crate rocket_contrib;
+
+use rocket_contrib::templates::Template;
+
+#[derive(Serialize)]
+struct TemplateContext {
+    title: String
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    let context = TemplateContext{ title: String::from("Home") };
+    Template::render("includes/home", &context)
 }
 
 #[get("/about")]
-fn about() -> &'static str {
-    "This website does absolutely nothing important."
+fn about() -> Template {
+    let context = TemplateContext{ title: String::from("About") };
+    Template::render("includes/about", &context)
+}
+
+#[catch(404)]
+fn page_not_found() -> Template {
+    let context = TemplateContext{ title: String::from("404 Page Not Found") };
+    Template::render("errors/404", &context)
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, about])
+        .attach(Template::fairing())
+        .register(catchers![page_not_found])
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, about]).launch();
+    rocket().launch();
 }
